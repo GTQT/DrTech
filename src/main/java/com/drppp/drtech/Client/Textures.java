3c28aff6a315951fe00bd0a8ab724b7c5798d19f
Dr-ppp <672749065@qq.com>
2024-02-04 10:54:43 +0800

    加入UU生产小机器和废料

--- src/main/java/com/drppp/drtech/Client/Textures.java
+++ src/main/java/com/drppp/drtech/Client/Textures.java
@@ -5,6 +5,7 @@ import gregtech.api.unification.material.info.MaterialIconSet;
 import gregtech.api.unification.material.info.MaterialIconType;
 import gregtech.api.util.GTLog;
 import gregtech.api.util.GTUtility;
+import gregtech.client.renderer.texture.cube.OrientedOverlayRenderer;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.util.ResourceLocation;
@@ -25,12 +26,14 @@ public class Textures {
     @SideOnly(Side.CLIENT)
     public static TextureAtlasSprite LASER_PIPE_OVERLAY_EMISSIVE;
     @SideOnly(Side.CLIENT)
+    public static OrientedOverlayRenderer UUPRODUCTER_OVERLAY;
+    @SideOnly(Side.CLIENT)
     public static void register(TextureMap textureMap) {
 
         LASER_PIPE_SIDE = textureMap.registerSprite(new ResourceLocation("drtech", "blocks/pipe/pipe_laser_side"));
         LASER_PIPE_IN = textureMap.registerSprite(new ResourceLocation("drtech", "blocks/pipe/pipe_laser_in"));
         LASER_PIPE_OVERLAY = textureMap.registerSprite(new ResourceLocation("drtech", "blocks/pipe/pipe_laser_side_overlay"));
         LASER_PIPE_OVERLAY_EMISSIVE = textureMap.registerSprite(new ResourceLocation("drtech", "blocks/pipe/pipe_laser_side_overlay_emissive"));
-
+        UUPRODUCTER_OVERLAY  = new OrientedOverlayRenderer("machines/uu_producter");
     }
 }
--- src/main/java/com/drppp/drtech/Load/MachineReceipe.java
+++ src/main/java/com/drppp/drtech/Load/MachineReceipe.java
@@ -2,7 +2,11 @@ package com.drppp.drtech.Load;
 
 import com.drppp.drtech.Items.ItemsInit;
 import com.drppp.drtech.Items.MetaItems.MyMetaItems;
+import gregtech.api.recipes.RecipeBuilder;
 import gregtech.api.recipes.RecipeMaps;
+import gregtech.api.recipes.ingredients.nbtmatch.NBTCondition;
+import gregtech.api.recipes.ingredients.nbtmatch.NBTMatcher;
+import gregtech.api.recipes.recipeproperties.ScanProperty;
 import gregtech.api.unification.OreDictUnifier;
 import gregtech.api.unification.material.Material;
 import gregtech.api.unification.material.Materials;
@@ -21,14 +25,18 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
 import  com.drppp.drtech.Blocks.BlocksInit;
+import net.minecraft.nbt.NBTTagCompound;
 
 import static com.drppp.drtech.Load.DrtechReceipes.EIMPLOSION_RECIPES;
+import static com.drppp.drtech.Load.DrtechReceipes.UU_RECIPES;
 import static gregtech.api.recipes.RecipeMaps.*;
 import static gregtech.api.unification.material.Materials.Carbon;
 import static gregtech.api.unification.material.Materials.NetherStar;
 import static gregtech.api.unification.material.info.MaterialFlags.EXPLOSIVE;
 import static gregtech.api.unification.material.info.MaterialFlags.FLAMMABLE;
 import static gregtech.api.unification.ore.OrePrefix.dust;
+import static gregtech.api.unification.ore.OrePrefix.ingot;
+import static gregtech.api.util.AssemblyLineManager.writeResearchToNBT;
 
 public class MachineReceipe {
     public static void load()
@@ -60,6 +68,33 @@ public class MachineReceipe {
                 .explosivesAmount(OreDictUnifier.get(OrePrefix.dust, Carbon))
                 .buildAndRegister();
         OrePrefix.dust.addProcessingHandler(PropertyKey.DUST, MachineReceipe::processDust);
+        //160秒
+        UU_RECIPES.recipeBuilder()
+                .fluidInputs(Materials.Air.getFluid(10))
+                .fluidOutputs(Materials.UUMatter.getFluid(1))
+                .EUt(256)
+                .duration(3200)
+                .buildAndRegister();
+        UU_RECIPES.recipeBuilder()
+                .fluidInputs(Materials.Air.getFluid(1))
+                .input(MyMetaItems.SCRAP)
+                .fluidOutputs(Materials.UUMatter.getFluid(1))
+                .EUt(256)
+                .duration(1600)
+                .buildAndRegister();
+        ItemStack is = MetaItems.TOOL_DATA_STICK.getStackForm();
+        NBTTagCompound compound = GTUtility.getOrCreateNbtCompound(MetaItems.ELECTRIC_MOTOR_LV.getStackForm());
+        writeResearchToNBT(compound, "1xmetaitem.electric.motor.lv@127");
+        is.setTagCompound(compound);
+        RecipeBuilder<?> builder= SCANNER_RECIPES.recipeBuilder()
+                 .inputNBT(is.getItem(), 1,is.getMetadata(),NBTMatcher.ANY, NBTCondition.ANY)
+                 .input(MetaItems.ELECTRIC_MOTOR_LV)
+                 .outputs(is)
+                 .duration(100)
+                 .EUt(2);
+        builder.applyProperty(ScanProperty.getInstance(), true);
+        builder.buildAndRegister();
+
     }
     public static void processDust(OrePrefix dustPrefix, Material mat, DustProperty property)
     {
@@ -73,13 +108,6 @@ public class MachineReceipe {
                         .outputs(GTUtility.copy(3, gemStack))
                         .chancedOutput(dust, Materials.DarkAsh, 2500, 0)
                        .explosivesAmount(dustStack)
-                        .buildAndRegister();
-
-                EIMPLOSION_RECIPES.recipeBuilder()
-                        .inputs(GTUtility.copy(3, dustStack))
-                        .outputs(GTUtility.copy(3, gemStack))
-                        .chancedOutput(dust, Materials.DarkAsh, 2500, 0)
-                        .explosivesAmount(MetaItems.DYNAMITE.getStackForm())
                         .buildAndRegister();
             }
         }
