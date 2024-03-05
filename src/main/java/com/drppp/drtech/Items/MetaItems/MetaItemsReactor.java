d2355ad6de6c46f50cd8af52310a6d2d5365b341
Dr-ppp <672749065@qq.com>
2024-03-05 22:05:10 +0800

    燃料棒

---
+++ src/main/java/com/drppp/drtech/Items/MetaItems/MetaItemsReactor.java
@@ -0,0 +1,41 @@
+package com.drppp.drtech.Items.MetaItems;
+
+import gregtech.api.items.metaitem.MetaItem;
+
+public class MetaItemsReactor {
+    public static MetaItem<?>.MetaValueItem U_FUEL_ROD_1X ;
+    public static MetaItem<?>.MetaValueItem U_FUEL_ROD_2X ;
+    public static MetaItem<?>.MetaValueItem U_FUEL_ROD_4X ;
+    public static MetaItem<?>.MetaValueItem U_FUEL_ROD_1X_EX ;
+    public static MetaItem<?>.MetaValueItem U_FUEL_ROD_2X_EX ;
+    public static MetaItem<?>.MetaValueItem U_FUEL_ROD_4X_EX ;
+    public static MetaItem<?>.MetaValueItem Th_FUEL_ROD_1X ;
+    public static MetaItem<?>.MetaValueItem Th_FUEL_ROD_2X ;
+    public static MetaItem<?>.MetaValueItem Th_FUEL_ROD_4X ;
+    public static MetaItem<?>.MetaValueItem Th_FUEL_ROD_1X_EX ;
+    public static MetaItem<?>.MetaValueItem Th_FUEL_ROD_2X_EX ;
+    public static MetaItem<?>.MetaValueItem Th_FUEL_ROD_4X_EX ;
+    public static MetaItem<?>.MetaValueItem HEAT_VENT;
+    public static MetaItem<?>.MetaValueItem ADVANCED_HEAT_VENT;
+    public static MetaItem<?>.MetaValueItem REACTOR_HEAT_VENT;
+    public static MetaItem<?>.MetaValueItem COMPONENT_HEAT_VENT;
+    public static MetaItem<?>.MetaValueItem OVERCLOCKED_HEAT_VENT;
+    public static MetaItem<?>.MetaValueItem HEAT_EXCHANGER;
+    public static MetaItem<?>.MetaValueItem ADVANCED_HEAT_EXCHANGER;
+    public static MetaItem<?>.MetaValueItem COMPONENT_HEAT_EXCHANGER;
+    public static MetaItem<?>.MetaValueItem REACTOR_HEAT_EXCHANGER;
+    public static MetaItem<?>.MetaValueItem NEUTRON_REFLECTOR1;
+    public static MetaItem<?>.MetaValueItem THICK_NEUTRON_REFLECTOR;
+    public static MetaItem<?>.MetaValueItem IRIDIUM_NEUTRON_REFLECTOR;
+    public static MetaItem<?>.MetaValueItem HE_COOLANT_CELL_60K;
+    public static MetaItem<?>.MetaValueItem HE_COOLANT_CELL_180K;
+    public static MetaItem<?>.MetaValueItem HE_COOLANT_CELL_360K;
+    public static MetaItem<?>.MetaValueItem NAK_COOLANT_CELL_60K;
+    public static MetaItem<?>.MetaValueItem NAK_COOLANT_CELL_180K;
+    public static MetaItem<?>.MetaValueItem NAK_COOLANT_CELL_360K;
+    public static void FuelRodInit()
+    {
+        MetaItemsReactorReg metaItemFuelRod = new MetaItemsReactorReg();
+        metaItemFuelRod.setRegistryName("meta_items_fuel_rod");
+    }
+}
