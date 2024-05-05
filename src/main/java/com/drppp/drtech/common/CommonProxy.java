b21ca3d93b067653018c6943aee3cdc03c37c6ee
KeQing <1158677939@qq.com>
2024-05-05 21:15:42 +0800

    Update CommonProxy.java

--- src/main/java/com/drppp/drtech/common/CommonProxy.java
+++ src/main/java/com/drppp/drtech/common/CommonProxy.java
@@ -2,6 +2,9 @@ package com.drppp.drtech.common;
 
 import com.drppp.drtech.Tags;
 import com.drppp.drtech.api.event.MobHordeEvent;
+import net.minecraft.entity.monster.EntityCreeper;
+import net.minecraft.entity.monster.EntityShulker;
+import net.minecraft.entity.monster.EntitySkeleton;
 import net.minecraft.entity.monster.EntityZombie;
 import net.minecraftforge.fml.common.Mod;
 
@@ -11,9 +14,12 @@ public class CommonProxy {
 
     }
     public void load() {
-        new MobHordeEvent((p) -> new EntityZombie(p.world), 4, 8, "zombies_a").setMaximumDistanceUnderground(10).setNightOnly(true);
-        new MobHordeEvent((p) -> new EntityZombie(p.world), 8, 16, "zombies_b").setMaximumDistanceUnderground(15).setNightOnly(true);
-        new MobHordeEvent((p) -> new EntityZombie(p.world), 16, 32, "zombies_c").setMaximumDistanceUnderground(20).setNightOnly(true);
-        new MobHordeEvent((p) -> new EntityZombie(p.world), 32, 64, "zombies_d").setMaximumDistanceUnderground(25).setNightOnly(true);
+        int i;
+        for(i=1;i<=4;i++) {
+            new MobHordeEvent((p) -> new EntityZombie(p.world), (int)Math.pow(2,i), (int)Math.pow(2,i)*2, String.format("zombies_%s", i)).setMaximumDistanceUnderground(i * 5).setNightOnly(true);
+            new MobHordeEvent((p) -> new EntitySkeleton(p.world), (int)Math.pow(2,i), (int)Math.pow(2,i)*2, String.format("skeleton_%s", i)).setMaximumDistanceUnderground(i * 5).setNightOnly(true);
+            new MobHordeEvent((p) -> new EntityCreeper(p.world), (int)Math.pow(2,i), (int)Math.pow(2,i)*2, String.format("creeper_%s", i)).setMaximumDistanceUnderground(i * 5).setNightOnly(true);
+
+        }
     }
 }
