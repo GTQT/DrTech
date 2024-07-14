32a3470832a9448eba5987178eb788c2b7166877
KeQing <1158677939@qq.com>
2024-07-14 11:21:49 +0800

    袭击削弱

--- src/main/java/com/drppp/drtech/common/CommonProxy.java
+++ src/main/java/com/drppp/drtech/common/CommonProxy.java
@@ -17,7 +17,7 @@ public class CommonProxy {
     public void load() {
         if(DrtConfig.MobHordeEvent) {
             int i;
-            for (i = 1; i <= 4; i++) {
+            for (i = 1; i <= 1; i++) {
                 if(DrtConfig.ZombieHordeEvent)new MobHordeEvent((p) -> new EntityZombie(p.world), (int) Math.pow(2, i), (int) Math.pow(2, i) * 2, String.format("zombies_%s", i)).setMaximumDistanceUnderground(i * 5).setNightOnly(true);
                 if(DrtConfig.SkeletonMobHordeEvent)new MobHordeEvent((p) -> new EntitySkeleton(p.world), (int) Math.pow(2, i), (int) Math.pow(2, i) * 2, String.format("skeleton_%s", i)).setMaximumDistanceUnderground(i * 5).setNightOnly(true);
                 if(DrtConfig.CreeperMobHordeEvent)new MobHordeEvent((p) -> new EntityCreeper(p.world), (int) Math.pow(2, i), (int) Math.pow(2, i) * 2, String.format("creeper_%s", i)).setMaximumDistanceUnderground(i * 5).setNightOnly(true);
