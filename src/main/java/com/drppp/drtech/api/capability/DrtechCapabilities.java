764d5c834c22dc8956389032133ba5ce563aea42
Dr-ppp <67527396+Dr-ppp@users.noreply.github.com>
2024-09-08 02:23:58 +0800

    yot仓全bug版

--- src/main/java/com/drppp/drtech/api/capability/DrtechCapabilities.java
+++ src/main/java/com/drppp/drtech/api/capability/DrtechCapabilities.java
@@ -7,12 +7,13 @@ import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.multiblock.MultiblockAbility;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityInject;
+import net.minecraftforge.fluids.IFluidTank;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class DrtechCapabilities {
-    public static final MultiblockAbility<IAEFluidStack> YOT_HATCH = new MultiblockAbility("yot_hatch");
+    public static final MultiblockAbility<IFluidTank> YOT_HATCH = new MultiblockAbility("yot_hatch");
 
     @CapabilityInject(INuclearDataShow.class)
     public static Capability<INuclearDataShow> CAPABILITY_NUCLEAR_DATA = null;
