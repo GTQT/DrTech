c007e4b5d987638e0ce58757691c77eb8e0d989e
Dr-ppp <67527396+Dr-ppp@users.noreply.github.com>
2024-12-16 22:40:15 +0800

    配方添加、燃料棒换电路

--- src/main/java/com/drppp/drtech/intergations/GtqtCoreLinkage.java
+++ src/main/java/com/drppp/drtech/intergations/GtqtCoreLinkage.java
@@ -8,6 +8,7 @@ import net.minecraft.item.ItemStack;
 
 import static com.drppp.drtech.loaders.DrtechReceipes.LOG_CREATE;
 import static gregtech.api.unification.material.Materials.Helium;
+import static gregtech.api.unification.material.Materials.SodiumPotassium;
 
 
 public class GtqtCoreLinkage {
@@ -39,6 +40,28 @@ public class GtqtCoreLinkage {
                 .input(MetaItemsReactor.COOLANT_NULL_CELL_3)
                 .fluidInputs(Helium.getFluid(FluidStorageKeys.LIQUID, 6000))
                 .output(MetaItemsReactor.HE_COOLANT_CELL_360K)
+                .EUt(1)
+                .duration(60)
+                .buildAndRegister();
+
+        GTQTcoreRecipeMaps.FLUID_CANNER_RECIPES.recipeBuilder()
+                .input(MetaItemsReactor.COOLANT_NULL_CELL_1)
+                .fluidInputs(SodiumPotassium.getFluid(FluidStorageKeys.LIQUID, 1000))
+                .output(MetaItemsReactor.NAK_COOLANT_CELL_60K)
+                .EUt(1)
+                .duration(10)
+                .buildAndRegister();
+        GTQTcoreRecipeMaps.FLUID_CANNER_RECIPES.recipeBuilder()
+                .input(MetaItemsReactor.COOLANT_NULL_CELL_2)
+                .fluidInputs(SodiumPotassium.getFluid(FluidStorageKeys.LIQUID, 3000))
+                .output(MetaItemsReactor.NAK_COOLANT_CELL_180K)
+                .EUt(1)
+                .duration(30)
+                .buildAndRegister();
+        GTQTcoreRecipeMaps.FLUID_CANNER_RECIPES.recipeBuilder()
+                .input(MetaItemsReactor.COOLANT_NULL_CELL_3)
+                .fluidInputs(SodiumPotassium.getFluid(FluidStorageKeys.LIQUID, 6000))
+                .output(MetaItemsReactor.NAK_COOLANT_CELL_360K)
                 .EUt(1)
                 .duration(60)
                 .buildAndRegister();
